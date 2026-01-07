b42d518dd8440f749a5d15b45b8d3523f9e45ad2
Abshar777 <absharameen625@gmail.com>
2026-01-07 20:18:43 +0530

    chore: Add pre_start build command to ecosystem config.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,12 @@ module.exports = {
       script: "npm",
       args: "start",
       cwd: "/clt-landing",
+      pre_start: "npm run build",
       exec_mode: "fork",
       instances: 1,
       env: {
         NODE_ENV: "production",
-      }
-    }
-  ]
+      },
+    },
+  ],
 };
